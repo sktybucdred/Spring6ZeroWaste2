15f9e529f7a14260a9c63c9f9151b2168ee07626
Jakub Wyszynski <wyszynski.jakub03@gmail.com>
2024-11-24 20:19:18 +0100

    przypisanie taskow do user

--- src/main/java/projekt/zespolowy/zero_waste/controller/TaskController.java
+++ src/main/java/projekt/zespolowy/zero_waste/controller/TaskController.java
@@ -14,25 +14,22 @@ public class TaskController {
     @Autowired
     private TaskService taskService;
 
-    // Display the form for creating a new task
     @GetMapping("/newTask")
     public String createTaskForm(Model model) {
         model.addAttribute("task", new Task());
         return "newTask";
     }
 
-    // Handle the creation of a new task and assign it to users
     @PostMapping("/newTask")
     public String createTask(@ModelAttribute Task task) {
-        taskService.createAndAssignNewTask(task); // Call service to save the task and assign it to users
-        return "redirect:/tasks/showAllTasks"; // Redirect to the task list after creating the task
+        taskService.createAndAssignNewTask(task);
+        return "redirect:/tasks/showAllTasks";
     }
 
-    // Display the list of all tasks
     @GetMapping("/showAllTasks")
     public String listTasks(Model model) {
-        model.addAttribute("tasks", taskService.getAllTasks()); // Add all tasks to the model
-        return "showAllTasks"; // Returns the task.html template to display the list of tasks
+        model.addAttribute("tasks", taskService.getAllTasks());
+        return "showAllTasks";
     }
 
     @GetMapping("/delete/{id}")
