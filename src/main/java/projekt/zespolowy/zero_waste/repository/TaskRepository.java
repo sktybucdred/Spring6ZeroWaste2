f3b24d4b721f31c004bc0fb446ea89ab077ed54f
Jakub Wyszynski <wyszynski.jakub03@gmail.com>
2024-12-02 15:23:51 +0100

    przykladowe przyznawanie punktow za task

--- src/main/java/projekt/zespolowy/zero_waste/repository/TaskRepository.java
+++ src/main/java/projekt/zespolowy/zero_waste/repository/TaskRepository.java
@@ -1,6 +1,8 @@
 package projekt.zespolowy.zero_waste.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import projekt.zespolowy.zero_waste.entity.Task;
 import projekt.zespolowy.zero_waste.entity.TaskType;
@@ -10,14 +12,11 @@ import java.util.List;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
-//    // Znajdź zadania o określonym typie
-//    List<Task> findByTaskType(TaskType taskType);
-//
-//    // Znajdź wszystkie zadania o określonym zakresie dat
-//    List<Task> findByStartDateBetween(LocalDate startDate, LocalDate endDate);
-//
-//    // Znajdź zadania o punktach większych niż
-//    List<Task> findByPointsAwardedGreaterThan(int points);
+    // Znajdź zadania o określonym typie
+    List<Task> findByTaskType(TaskType taskType);
+
+    @Query("SELECT t FROM Task t WHERE t.task_name = :taskName")
+    Task findByTaskName(@Param("taskName") String taskName);
 
     List<Task> findAll();
 }
