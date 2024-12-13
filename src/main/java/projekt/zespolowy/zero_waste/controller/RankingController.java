063215f7653f26abd7cbd1bb95c89bf56aa32c1f
Jakub Wyszynski <wyszynski.jakub03@gmail.com>
2024-12-13 13:39:37 +0100

    przekierowanie do profilow w rankingu

--- src/main/java/projekt/zespolowy/zero_waste/controller/RankingController.java
+++ src/main/java/projekt/zespolowy/zero_waste/controller/RankingController.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import projekt.zespolowy.zero_waste.entity.User;
 import projekt.zespolowy.zero_waste.security.CustomUser;
@@ -50,7 +51,41 @@ public class RankingController {
         model.addAttribute("search", search);
         model.addAttribute("sortBy", sortBy);
         model.addAttribute("userRank", userRank);
+        model.addAttribute("loggedUserId", loggedUser.getId());
+        System.out.println(loggedUser.getId());
 
         return "ranking";
+    }
+
+    @GetMapping("/user/profile/{userId}")
+    public String showUserProfile(@PathVariable Long userId, Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        CustomUser customUser = (CustomUser) authentication.getPrincipal();
+        User loggedUser = customUser.getUser();
+
+        User user = userService.findById(userId);
+
+        if (user == null) {
+            return "redirect:/error";
+        }
+
+        if (loggedUser.getId().equals(userId)) {
+            return "redirect:/account";
+        }
+
+        model.addAttribute("user", user);
+
+        return "user-profile";
+    }
+
+    @GetMapping("/account")
+    public String accountDetails(Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        CustomUser customUser = (CustomUser) authentication.getPrincipal();
+        User user = customUser.getUser();
+
+        model.addAttribute("user", user);
+
+        return "User/accountDetails";
     }
 }
--- src/main/java/projekt/zespolowy/zero_waste/services/CustomOidcUserService.java
+++ src/main/java/projekt/zespolowy/zero_waste/services/CustomOidcUserService.java
@@ -1,18 +1,24 @@
 package projekt.zespolowy.zero_waste.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import projekt.zespolowy.zero_waste.entity.Task;
 import projekt.zespolowy.zero_waste.entity.User;
+import projekt.zespolowy.zero_waste.entity.UserTask;
 import projekt.zespolowy.zero_waste.entity.enums.AccountType;
 import projekt.zespolowy.zero_waste.entity.enums.AuthProvider;
+import projekt.zespolowy.zero_waste.repository.TaskRepository;
 import projekt.zespolowy.zero_waste.repository.UserRepository;
+import projekt.zespolowy.zero_waste.repository.UserTaskRepository;
 import projekt.zespolowy.zero_waste.security.CustomUser;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -20,6 +26,12 @@ public class CustomOidcUserService extends OidcUserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @Autowired
+    private UserTaskRepository userTaskRepository;
 
     public CustomOidcUserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
@@ -63,6 +75,21 @@ public class CustomOidcUserService extends OidcUserService {
 
             // Zapisz użytkownika w bazie danych
             userRepository.save(user);
+
+            List<Task> allTasks = taskRepository.findAll();
+
+            // Dla każdego zadania przypisz je do nowego użytkownika
+            for (Task task : allTasks) {
+                UserTask userTask = new UserTask();
+                userTask.setUser(user);
+                userTask.setTask(task);
+                userTask.setProgress(0);
+                userTask.setCompleted(false);
+                userTask.setCompletionDate(null);
+
+                // Zapisz obiekt UserTask w bazie
+                userTaskRepository.save(userTask);
+            }
         }
 
         // Zwróć CustomUser z atrybutami OIDC
