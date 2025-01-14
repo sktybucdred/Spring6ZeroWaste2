2c05410c7be181369bc9cfe7dedf35862f820757
Mateusz Zawadzki <86227@student.pb.edu.pl>
2025-01-14 23:00:02 +0100

    Dodanie wyszukiwania (PZ2024WAS-20)

--- src/main/java/projekt/zespolowy/zero_waste/services/ProductService.java
+++ src/main/java/projekt/zespolowy/zero_waste/services/ProductService.java
@@ -10,24 +10,27 @@ import java.util.Optional;
 
 public interface ProductService {
 
-   List<Product>getAllProducts();
-    Page<Product> getAllProducts(Pageable pageable);
+ List<Product>getAllProducts();
+ Page<Product> getAllProducts(Pageable pageable);
 
-    Product saveProduct(Product product);
+ Product saveProduct(Product product);
 
-    Optional<Product> getProductById(Long id);
+ Optional<Product> getProductById(Long id);
 
-    void deleteProduct(Long id);
+ void deleteProduct(Long id);
 
-    List<Product> getProductsByIds(List<Long> ids);
-    Page<Product> getProductsByCategory(ProductCategory category, Pageable pageable);
+ List<Product> getProductsByIds(List<Long> ids);
+ Page<Product> getProductsByCategory(ProductCategory category, Pageable pageable);
 
-    Page<Product> getAllProductsSortedByPriceAsc(Pageable pageable);
-    Page<Product> getAllProductsSortedByPriceDesc(Pageable pageable);
+ Page<Product> getProductsByNameContainingIgnoreCaseSortedByPriceAsc(String search, Pageable pageable);
+ Page<Product> getProductsByNameContainingIgnoreCaseSortedByPriceDesc(String search, Pageable pageable);
+ Page<Product> getProductsByNameContainingIgnoreCaseSortedByDateDesc(String search, Pageable pageable);
 
-    Page<Product> getProductsByCategorySortedByPriceAsc(ProductCategory category, Pageable pageable);
-    Page<Product> getProductsByCategorySortedByPriceDesc(ProductCategory category, Pageable pageable);
-    Page<Product> getAllProductsSortedByDateDesc(Pageable pageable);
-    Page<Product> getProductsByCategorySortedByDateDesc(ProductCategory category, Pageable pageable);
+ Page<Product> getProductsByCategoryAndNameContainingIgnoreCaseSortedByPriceAsc(ProductCategory category, String search, Pageable pageable);
+ Page<Product> getProductsByCategoryAndNameContainingIgnoreCaseSortedByPriceDesc(ProductCategory category, String search, Pageable pageable);
+ Page<Product> getProductsByCategoryAndNameContainingIgnoreCaseSortedByDateDesc(ProductCategory category, String search, Pageable pageable);
+
+
+
 
 }
